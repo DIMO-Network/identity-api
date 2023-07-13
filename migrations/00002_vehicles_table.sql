33d6856450edd1ff949ee6fce21cff967e60b257
Dylan Moreland <dylan@dimo.zone>
2023-07-13 19:23:08 -0400

    pgFormatterg

--- migrations/00002_vehicles_table.sql
+++ migrations/00002_vehicles_table.sql
@@ -1,22 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
-
-CREATE TABLE vehicles(
-    id int,
-    owner_address bytea null
-        CONSTRAINT minted_vehicles_owner_address_check CHECK (length(owner_address) = 20),
-    make         varchar(100) null,
-    model        varchar(100) null,
-    year         int    null,
-    mint_time   timestamptz null,
-
-    PRIMARY KEY (id)
+CREATE TABLE vehicles (
+    id int CONSTRAINT vehicles_pkey PRIMARY KEY,
+    owner_address bytea CONSTRAINT vehicles_owner_address_check CHECK (length(owner_address) = 20),
+    make varchar(100),
+    model varchar(100),
+    year int,
+    mint_time timestamptz,
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-
-drop table vehicles;
-
+DROP TABLE vehicles;
 -- +goose StatementEnd
