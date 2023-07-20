f1024eb654f8c8dbaecd032c4cde40d72c34778e
0xdev22 <doyin@dimo.zone>
2023-07-20 13:15:07 -0400

    Fix issues from PR review

--- migrations/00005_update_mint_date.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE vehicles ALTER COLUMN minted_at SET NOT NULL;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE vehicles ALTER COLUMN minted_at DROP NOT NULL;
--- +goose StatementEnd
