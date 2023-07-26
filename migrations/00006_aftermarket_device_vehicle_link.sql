34a9aa906dbd722ea1a2d8e1743ccc84ad4837b3
Dylan Moreland <dylan@dimo.zone>
2023-07-26 17:36:39 -0400

    Hit it with the formatter

--- migrations/00006_aftermarket_device_vehicle_link.sql
+++ migrations/00006_aftermarket_device_vehicle_link.sql
@@ -1,20 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-
 ALTER TABLE aftermarket_devices
-ADD COLUMN vehicle_id int UNIQUE;
-
-ALTER TABLE aftermarket_devices
-ADD CONSTRAINT ad_vehicle_link FOREIGN KEY (vehicle_id) REFERENCES vehicles (id);
-
+    ADD COLUMN vehicle_id int CONSTRAINT aftermarket_devices_vehicle_id_key UNIQUE CONSTRAINT aftermarket_devices_vehicle_id_fkey REFERENCES vehicles (id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-
 ALTER TABLE aftermarket_devices
-DROP CONSTRAINT ad_vehicle_link;
-
-ALTER TABLE aftermarket_devices
-DROP COLUMN vehicle_id;
+    DROP COLUMN vehicle_id;
 -- +goose StatementEnd
