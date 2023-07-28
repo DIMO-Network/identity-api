4fbd316282e7ccf6c9d04a76a4e90ae5444a300b
Dylan Moreland <dylan@dimo.zone>
2023-07-28 12:04:52 -0400

    Minor SQL reformat

--- migrations/00006_privileges_table.sql
+++ migrations/00006_privileges_table.sql
@@ -1,14 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE privileges (
-    id         char(27) PRIMARY KEY,
-    token_id int NOT NULL,
+    id varchar PRIMARY KEY,
+    token_id int NOT NULL CONSTRAINT vehicle_privileges_vehicle_token_id REFERENCES vehicles (id),
     privilege_id int NOT NULL,
-    user_address bytea CONSTRAINT user_address_check CHECK (length(user_address) = 20) NOT NULL,
+    user_address bytea CONSTRAINT vehicle_privileges_user_address_check CHECK (length(user_address) = 20) NOT NULL,
     set_at timestamptz NOT NULL,
-    expires_at timestamptz NOT NULL,
-
-    CONSTRAINT fkey_privileges_token_id FOREIGN KEY (token_id) REFERENCES vehicles(id)
+    expires_at timestamptz NOT NULL
 );
 -- +goose StatementEnd
 
