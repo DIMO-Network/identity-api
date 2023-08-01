63c1dafc5bffdf5f1da0b2e78d58d2f41c70e051
Dylan Moreland <dylan@dimo.zone>
2023-08-01 14:07:18 -0400

    Format jerk

--- migrations/00008_set_device_beneficiary.sql
+++ migrations/00008_set_device_beneficiary.sql
@@ -1,14 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-
 ALTER TABLE aftermarket_devices
-ADD COLUMN beneficiary bytea CONSTRAINT beneficiary_address_check CHECK (length(beneficiary) = 20) NOT NULL;
-
+    ADD COLUMN beneficiary bytea NOT NULL CONSTRAINT aftermarket_devices_beneficiary_check CHECK (length(beneficiary) = 20);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-
 ALTER TABLE aftermarket_devices
-DROP COLUMN beneficiary;
+    DROP COLUMN beneficiary;
 -- +goose StatementEnd
