88a07d3f456c4b3fc340009431dc372660387f60
Doyin.O <111298305+0xdev22@users.noreply.github.com>
2023-09-19 13:27:11 -0400

    Update migrations/00015_dcn_vehicle_id_column.sql
    Co-authored-by: Dylan Moreland <79415431+elffjs@users.noreply.github.com>

--- migrations/00015_dcn_vehicle_id_column.sql
+++ migrations/00015_dcn_vehicle_id_column.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE dcns ADD COLUMN vehicle_id int CONSTRAINT vehicle_dcn_vehicle_token_id REFERENCES vehicles (id);
+ALTER TABLE dcns ADD COLUMN vehicle_id int CONSTRAINT dcns_vehicle_id_fkey REFERENCES vehicles (id);
 -- +goose StatementEnd
 
 -- +goose Down
