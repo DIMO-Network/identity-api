6410d003644679ea257f14f047ad5e2c34318166
Dylan Moreland <dylan@dimo.zone>
2023-10-06 14:36:11 -0400

    Typo

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -124,18 +124,3 @@ type aftermarketDeviceResolver struct{ *Resolver }
 type dCNResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type vehicleResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *dcnResolver) Vehicle(ctx context.Context, obj *model.Dcn) (*model.Vehicle, error) {
-	if obj.VehicleID == nil {
-		return nil, nil
-	}
-	return loader.GetVehicleByID(ctx, *obj.VehicleID)
-}
-
-type dcnResolver struct{ *Resolver }
