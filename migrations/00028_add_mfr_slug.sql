a8de43c327b9cc4aad3be396711d36198d874272
Dylan Moreland <dylan@dimo.zone>
2024-05-12 07:33:41 -0400

    Don't error if we already have the extension

--- migrations/00028_add_mfr_slug.sql
+++ migrations/00028_add_mfr_slug.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE EXTENSION unaccent;
+CREATE EXTENSION IF NOT EXISTS unaccent;
 
 ALTER TABLE manufacturers
     ADD COLUMN slug text CONSTRAINT manufacturers_slug_key UNIQUE;
@@ -8,12 +8,10 @@ ALTER TABLE manufacturers
 UPDATE
     manufacturers
 SET
-    slug = lower(unaccent (replace(name, ' ', '-')));
+    slug = lower(unaccent(replace(name, ' ', '-')));
 
 ALTER TABLE manufacturers
     ALTER COLUMN slug SET NOT NULL;
-
-DROP EXTENSION unaccent;
 -- +goose StatementEnd
 
 -- +goose Down
