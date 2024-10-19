47fda79fb0e75300a4370d8f71c40028b385b67d
Dylan Moreland <dylan@dimo.zone>
2024-10-19 17:27:45 -0400

    License table migration

---
+++ migrations/00035_create_developer_license.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE developer_licenses(
+    token_id int NOT NULL CONSTRAINT developer_licenses_pkey PRIMARY KEY,
+    owner bytea NOT NULL CONSTRAINT developer_licenses_owner_check CHECK (length(owner) == 20),
+    address bytea NOT NULL CONSTRAINT developer_licenses_address_check CHECK (length(address) == 20) CONSTRAINT developer_licenses_address_key UNIQUE,
+    minted_at timestamptz NOT NULL,
+    alias text CONSTRAINT developer_licenses_alias_key UNIQUE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE developer_licenses;
+-- +goose StatementEnd
