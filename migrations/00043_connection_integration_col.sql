f379916d1a5ad6424eb98747c507792b395d9bec
Dylan Moreland <dylan@dimo.zone>
2025-05-26 18:56:31 -0400

    Add foreign key for connections
    We're using name. This will be controversial, but it's readable and
    as or more performant than the other options.

---
+++ migrations/00043_connection_integration_col.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE connections
+    ADD CONSTRAINT CHECK (octet_length(name) <= 32)
+    ADD COLUMN integration_id int;
+
+ALTER TABLE synthetic_devices
+    ADD COLUMN connection_name text CHECK (octet_length(connection_name) <= 32) REFERENCES connections (name);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE synthetic_devices
+    DROP COLUMN connection_address;
+
+ALTER TABLE connections
+    DROP COLUMN integration_id;
+-- +goose StatementEnd
+
+
+
+
+
