4a2c1fe917a619b3b24a57301d2021e7bbf4e1e5
Dylan Moreland <dylan@dimo.zone>
2025-07-18 00:35:31 -0400

    Add a translator from SQL row to GraphQL object

---
+++ internal/repositories/node/node.go
@@ -0,0 +1,35 @@
+package node
+
+import (
+	"math/big"
+
+	"github.com/DIMO-Network/cloudevent"
+	gmodel "github.com/DIMO-Network/identity-api/graph/model"
+	"github.com/DIMO-Network/identity-api/models"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type Repository struct {
+	chainID         uint64
+	contractAddress common.Address
+}
+
+func (r *Repository) ToAPI(v *models.StorageNode) *gmodel.StorageNode {
+	tokenID := new(big.Int).SetBytes(v.ID)
+
+	tokenDID := cloudevent.ERC721DID{
+		ChainID:         r.chainID,
+		ContractAddress: r.contractAddress,
+		TokenID:         tokenID,
+	}.String()
+
+	return &gmodel.StorageNode{
+		TokenID:  tokenID,
+		TokenDID: tokenDID,
+		Label:    v.Label,
+		Owner:    common.BytesToAddress(v.Owner),
+		Address:  common.BytesToAddress(v.Address),
+		URI:      v.URI,
+		MintedAt: v.MintedAt,
+	}
+}
